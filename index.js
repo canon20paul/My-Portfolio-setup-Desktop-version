24d590b40fe6c7c87c68be6dc1a799a6d669c156
canon20paul <canon20paul@gmail.com>
2022-07-30 13:01:31 +0300

    JS

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
-  const hamburger = document.querySelector('.hamburger');
-  const navmenu = document.querySelector('.nav-menu');
-  const navB = document.getElementById('email');
+const hamburger = document.querySelector('.hamburger');
+const navmenu = document.querySelector('.nav-menu');
+const navB = document.getElementById('email');
   hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navmenu.classList.toggle('active');
-    if (document.getElementById('email').style.zIndex == '-1') {
-      document.getElementById('email').style.zIndex = '1';
-      document.getElementById('names').style.zIndex = '1';
-      document.body.style.overflow = 'auto';
-      document.getElementById('navB').style.opacity = '1';
+  hamburger.classList.toggle('active');
+  navmenu.classList.toggle('active');
+  if (document.getElementById('email').style.zIndex === '-1') {
+    document.getElementById('email').style.zIndex = '1';
+    document.getElementById('names').style.zIndex = '1';
+    document.body.style.overflow = 'auto';
+    document.getElementById('navB').style.opacity = '1';
     } else {
-      document.getElementById('email').style.zIndex = '-1';
-      document.getElementById('names').style.zIndex = '-1';
-      document.body.style.overflow = 'hidden';
-      document.getElementById('navB').style.opacity = '0';
-    }
+    document.getElementById('email').style.zIndex = '-1';
+    document.getElementById('names').style.zIndex = '-1';
+    document.body.style.overflow = 'hidden';
+    document.getElementById('navB').style.opacity = '0';
   }
-);
+  });
   // Hide navMenu
 document.querySelectorAll('.nav-link').forEach((n) =>
-  n.addEventListener('click', () => {
+n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navmenu.classList.remove('active');
     document.getElementById('email').style.zIndex = '1';
     document.getElementById('email').style.zIndex = '1';
     document.getElementById('names').style.zIndex = '1';
     document.body.style.overflow = 'auto';
-    }
-  )
-);
+  }
+  ));
 document.getElementById('contactF').addEventListener('submit', (event) => {
   const email = document.getElementById('email').value;
   if (email !== email.toLowerCase()) {
-      event.preventDefault();
-     document.querySelector('.error').innerHTML ='Your email address should be in lowercase';
+    event.preventDefault();
+    document.querySelector('.error').innerHTML = 'Your email address should be in lowercase';
   }
-},
-);
+});
