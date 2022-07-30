26d5ef52e006652aaa5479a5134b76630458b15a
canon20paul <canon20paul@gmail.com>
2022-07-30 17:09:41 +0300

    Local Storage

--- dataStorage.js
+++ dataStorage.js
@@ -1,18 +1,31 @@
-const form = document.getElementById('ContactF');
-const Data = {
-  names: '',
-  email: '',
-  Naration: '',
-};
-form.addEventListener('change', () => {
-  Data.names = document.querySelector('#names').value;
-  Data.email = document.querySelector('#email').value;
-  Data.textArea = document.querySelector('#Naration').value;
-  localStorage.setItem('stored', JSON.stringify(Data));
-});
-const storedData = JSON.parse(localStorage.getItem('stored'));
-if (storedData) {
-  document.querySelector('#email').value = storedData.email;
-  document.querySelector('#names').value = storedData.names;
-  document.querySelector('#Naration').value = storedData.textArea;
+function recordD() {
+  const recordOBJ = JSON.parse(localStorage.getItem('recordOBJ'));
+  recordOBJ.names = document.querySelector('#names').value;
+  recordOBJ.email = document.querySelector('#email').value;
+  recordOBJ.Naration = document.querySelector('#Naration').value;
+  localStorage.setItem('recordOBJ', JSON.stringify(recordOBJ));
 }
+
+function check() {
+  if (!localStorage.getItem('recordOBJ')) {
+    const recordOBJ = {
+      names: '',
+      email: '',
+      Naration: '',
+    };
+    localStorage.setItem('recordOBJ', JSON.stringify(recordOBJ));
+  } else {
+    const recordOBJ = JSON.parse(localStorage.getItem('recordOBJ'));
+    document.querySelector('#names').value = recordOBJ.names;
+    document.querySelector('#email').value = recordOBJ.email;
+    document.querySelector('#Naration').value = recordOBJ.Naration;
+  }
+}
+
+window.onload = () => {
+  check(); 
+};
+
+document.querySelector('#names').addEventListener('keydown', recordD);
+document.querySelector('#email').addEventListener('keydown', recordD);
+document.querySelector('#Naration').addEventListener('keydown', recordD);
