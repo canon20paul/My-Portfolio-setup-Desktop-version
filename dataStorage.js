6f4534833db38469bce72292e25695b1844bba31
canon20paul <canon20paul@gmail.com>
2022-07-30 13:27:58 +0300

    JS

--- dataStorage.js
+++ dataStorage.js
@@ -9,10 +9,10 @@ form.addEventListener('change', () => {
   Data.email = document.querySelector('#email').value;
   Data.textArea = document.querySelector('#Naration').value;
   localStorage.setItem('stored', JSON.stringify(Data));
-  });
+});
 const storedData = JSON.parse(localStorage.getItem('stored'));
-  if (storedData) {
-  document.querySelector('#email').value = storedObject.email;
-  document.querySelector('#names').value = storedObject.names;
-  document.querySelector('#Naration').value = storedObject.textArea;
+if (storedData) {
+  document.querySelector('#email').value = storedData.email;
+  document.querySelector('#names').value = storedData.names;
+  document.querySelector('#Naration').value = storedData.textArea;
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const navmenu = document.querySelector('.nav-menu');
 const navB = document.getElementById('email');
-  hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navmenu.classList.toggle('active');
   if (document.getElementById('email').style.zIndex === '-1') {
@@ -9,24 +9,22 @@ const navB = document.getElementById('email');
     document.getElementById('names').style.zIndex = '1';
     document.body.style.overflow = 'auto';
     document.getElementById('navB').style.opacity = '1';
-    } else {
+} else {
     document.getElementById('email').style.zIndex = '-1';
     document.getElementById('names').style.zIndex = '-1';
     document.body.style.overflow = 'hidden';
     document.getElementById('navB').style.opacity = '0';
   }
-  });
-  // Hide navMenu
-document.querySelectorAll('.nav-link').forEach((n) =>
-n.addEventListener('click', () => {
+ });
+// Hide navMenu
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navmenu.classList.remove('active');
     document.getElementById('email').style.zIndex = '1';
     document.getElementById('email').style.zIndex = '1';
     document.getElementById('names').style.zIndex = '1';
     document.body.style.overflow = 'auto';
-  }
-  ));
+  }));
 document.getElementById('contactF').addEventListener('submit', (event) => {
   const email = document.getElementById('email').value;
   if (email !== email.toLowerCase()) {
